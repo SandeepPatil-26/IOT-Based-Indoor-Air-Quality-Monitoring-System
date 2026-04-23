cc49f1e6be33f6dab7819388d908aa386b1c578d
Sandeep Patil <sairam897078@gmai.com>
2026-04-23 12:16:08 +0530

    first commit : IOT-Based-Indoor-Air-Quality-Monitoring-System

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+int main() 
+{
+	
+   char s;
+   s=scanf("%ch",&s);
+   printf("Hello, World\n,&s",s);
+    return 0;
+}
